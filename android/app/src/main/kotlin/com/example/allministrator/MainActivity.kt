834b03f3f8a068d7d9586c20151cfe0681c1af65
josafat contreras <josafat005@outlook.com>
2026-07-13 23:39:33 -0600

    Firs apk export to make tests

--- android/app/src/main/kotlin/com/example/allministrator/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.allministrator
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/josaf/allministrator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.josaf.allministrator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
